5ef07d086863b23772bf2f1ba5d8eb3d0c4f39ba
darrenzeng <darrenzeng@tencent.com>
2025-03-21 19:26:39 +0800

    重写 list 数据容器

--- src/kernel/task/list.c
+++ src/kernel/task/list.c
@@ -1,63 +1,82 @@
 #include <task/list.h>
 
 void list_init(list_t* list) {
-    list->head = list->tail = nullptr;
+    list->head.previous = NULL;
+    list->tail.next = NULL;
+    list->head.next = &list->tail;
+    list->tail.previous = &list->head;
 }
 
 bool list_is_empty(list_t* list) {
-    return list->head == nullptr;
+    return (list->head.next == &list->tail);
 }
 
 void list_add_header(list_t* list, list_node_t* list_node) {
     assert(list_node != nullptr);
-    list_node->previous = list_node->next = nullptr;
-    if (list_is_empty(list)) {
-        list->head = list->tail = list_node;
-        return;
-    }
-    list_node->next = list->head;
-    list->head->previous = list_node;
-    list->head = list_node;
+    list_node->next = list->head.next;
+    list->head.next->previous = list_node;
+    list->head.next = list_node;
+    list_node->previous = &list->head;
 }
 
 void list_add_tail(list_t* list, list_node_t* list_node) {
     assert(list_node != nullptr);
-    if (list_is_empty(list)) {
-        list->head = list->tail = list_node;
-        return;
-    }
-    list_node->next = nullptr;
-    list_node->previous = list->tail;
-    list->tail->next = list_node;
-    list->tail = list_node;
+    list_node->next = &list->tail;
+    list->tail.previous->next = list_node;
+    list_node->previous = list->tail.previous;
+    list->tail.previous = list_node;
 }
 
 list_node_t* list_header(list_t* list) {
     assert(list != nullptr);
-    return list->head;
+    if (list_is_empty(list)) {
+        return nullptr;
+    }
+    return list->head.next;
 }
 
 list_node_t* list_tail(list_t* list) {
     assert(list != nullptr);
-    return list->tail;
-}
-
-list_node_t* list_remove_header(list_t* list) {
     if (list_is_empty(list)) {
         return nullptr;
     }
-    list_node_t* head = list->head;
-    list->head = head->next;
-    head->next = head->previous = nullptr;
+    return list->tail.previous;
+}
+
+list_node_t* list_remove_header(list_t* list) {
+    list_node_t* head = list->head.next;
+    list->head.next = head->next;
+    head->next->previous = &list->head;
     return head;
 }
 
 list_node_t* list_remove_tail(list_t* list) {
-    if (list_is_empty(list)) {
-        return nullptr;
-    }
-    list_node_t* tail = list->tail;
-    list->tail = tail->previous;
-    tail->next = tail->previous = nullptr;
+    list_node_t* tail = list->tail.previous;
+    list->tail.previous = tail->previous;
+    tail->previous->next = &list->tail;
     return tail;
+}
+
+// 移除一个节点
+bool list_remove(list_t* list, list_node_t* list_node){
+    assert(list != NULL && list_node != NULL);
+    list_node_t* current = list->head.next;
+    while (current)
+    {
+       if (current == list_node) {
+            // 找到了
+            break;
+       }
+       // 不断往下找
+       current = current->next;
+    }
+    if (current != list_node) {
+        // 没找到
+        return false;
+    }
+
+    // 移除掉当前节点
+    current->previous->next = current->next;
+    current->next->previous = current->previous;
+    return true;
 }
